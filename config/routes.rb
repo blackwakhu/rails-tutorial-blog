b945bef813bdb03890003620008236111a474034
shiberoderrickw <30028200-shiberoderrickw@users.noreply.replit.com>
2024-05-15 08:56:13 +0000

    added the models

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   get "/home", to: "home#index"
   get "/articles", to: "articles#index"
+  # get "/articles/all", to: "articles#all"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
