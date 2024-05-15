8a261a0a6f58406a54a59180ea2609828cf35cf6
shiberoderrickw <30028200-shiberoderrickw@users.noreply.replit.com>
2024-05-15 08:00:48 +0000

    added home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  
+  get "/", to: "home#index"
   get "/articles", to: "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
