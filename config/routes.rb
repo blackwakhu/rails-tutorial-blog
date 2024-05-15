c9cb6645a404a2581009988f019d5a95fb948a3c
shiberoderrickw <30028200-shiberoderrickw@users.noreply.replit.com>
2024-05-15 11:50:54 +0000

    added some pages to the rails project

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root "home#index"
   
   get "/home", to: "home#index"
+  get "/home/copies", to: "home#copies"
+  
   get "/articles", to: "articles#index"
-  # get "/articles/all", to: "articles#all"
+  get "/articles/:id", to: "articles#show"
+  get "/articles/all", to: "articles#all"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
